1012332053590f29215a24b5405591777b38ac69
kaixenberg <smarajitdatta6@gmail.com>
2026-08-21 01:51:15 +0530

    chore: bump version to 1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "our.bunny.julie"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0.10"
+        versionCode = 7
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
